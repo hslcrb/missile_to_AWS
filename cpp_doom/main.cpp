be6a4ced5be14b5c7f054711b6aa3a8ed15b4829
Rheehose <sea35652@gmail.com>
2026-03-18 02:06:06 +0900

    프로그램 창의 하단 높이를 한 번 더 줄이고, 터미널을 위로 바짝 끌어올려 가장 최적화된 컴팩트 사이즈를 완성했습니다!
    최종 조정 사항:
    
    터미널 크기 축소: 터미널 높이를 250에서 200으로 더 줄여, 꼭 필요한 정보만 집중해서 볼 수 있도록 했습니다.
    간격 최소화: 터미널 레이블(🖥️ TERMINAL)을 위쪽 NUKE 버튼에 더 가깝게(+10 -> +5) 밀착시켰습니다.
    창 높이 다이어트: 전체 창 높이를 820에서 750으로 최종 감축하여, 화면 공간을 낭비하지 않는 아주 날렵한 레이아웃이 되었습니다.
    이제 프로그램이 훨씬 더 균형 잡히고 목적에 집중된 모습을 갖추게 되었습니다. 확인해 보세요!

--- cpp_doom/main.cpp
+++ cpp_doom/main.cpp
@@ -482,11 +482,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         if (g_hNukeBmpDim) SendMessage(g_hBtnNuke, STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)g_hNukeBmpDim);
         
         // --- LOGS Section ---
-        int logsLblY = nukeY + nukeH + 10; // Moved up slightly
+        int logsLblY = nukeY + nukeH + 5; // Moved up closer
         CreateWindow(L"STATIC", L"🖥️ TERMINAL", WS_VISIBLE | WS_CHILD, 20, logsLblY, 200, 25, hwnd, NULL, NULL, NULL);
         
         int logsEditY = logsLblY + 25;
-        g_hLogs = CreateWindow(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL, 20, logsEditY, 550, 250, hwnd, (HMENU)ID_EDIT_LOGS, NULL, NULL);
+        g_hLogs = CreateWindow(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL, 20, logsEditY, 550, 200, hwnd, (HMENU)ID_EDIT_LOGS, NULL, NULL);
         g_OldEditProc = (WNDPROC)SetWindowLongPtr(g_hLogs, GWLP_WNDPROC, (LONG_PTR)TerminalEditProc);
 
         EnumChildWindows(hwnd, [](HWND child, LPARAM font) -> BOOL {
@@ -581,7 +581,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     RegisterClassEx(&wc);
 
-    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"missile_to_AWS", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 610, 820, NULL, NULL, hInstance, NULL);
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"missile_to_AWS", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 610, 750, NULL, NULL, hInstance, NULL);
     if (hwnd == NULL) return 0;
 
     ShowWindow(hwnd, nShowCmd);
