d2671eca9c3646a9e3462e17f906ea0f4ee50210
Rheehose <sea35652@gmail.com>
2026-03-26 21:16:31 +0900

    [REFACTOR] src 디렉토리 생성 및 소스코드 재배치

--- cpp_doom/build.bat
+++ cpp_doom/build.bat
@@ -1,4 +1,4 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-rc.exe resources.rc
-cl.exe /utf-8 /EHsc /O2 /DUNICODE /D_UNICODE main.cpp resources.res /Fe:AWSCleaner.exe user32.lib gdi32.lib comctl32.lib shell32.lib gdiplus.lib ole32.lib
+rc.exe /nologo /fo resources.res src\resources.rc
+cl.exe /nologo /utf-8 /EHsc /O2 /DUNICODE /D_UNICODE src\main.cpp resources.res /Fe:AWSCleaner.exe user32.lib gdi32.lib comctl32.lib shell32.lib gdiplus.lib ole32.lib
